9ab91db84f602d0084894f0c8f30ec301af248b7
Miguel Osorno <miguel.osornolinares@gmail.com>
2020-04-23 09:18:54 -0500

    se desarrollo logica para insertar una transaccion

---
+++ DatosCapa/DatosTransaccion.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data.SqlClient
+Public Class DatosTransaccion
+    Dim conectar As New conexion
+    Dim sql As New SqlCommand
+    Dim Da As New SqlDataAdapter
+    Dim dt As New DataTable
+
+    Public Sub Registrar_transaccion(ByVal ET As EntidadCapa.transaccion)
+        Try
+            conectar.conectando()
+            sql = New SqlCommand("INSERTAR_TRANSACCION", conectar.cnx)
+            sql.CommandType = CommandType.StoredProcedure
+            With sql.Parameters
+                .AddWithValue("@Fecha", ET.metodo_fechaTransaccion)
+                .AddWithValue("@IdProducto", ET.metodo_idProductoTrasaccion)
+                .AddWithValue("@IdProvedor", ET.metodo_idProvedorTransaccion)
+                .AddWithValue("@Cantidad", ET.metodo_cantidadTransaccion)
+                .AddWithValue("@Tipo", ET.metodo_tipoTransaccion)
+            End With
+            sql.ExecuteNonQuery()
+            MsgBox("Se insertaron los datos", MsgBoxStyle.Information)
+            conectar.desconectando()
+        Catch ex As Exception
+            MsgBox("" + ex.ToString, MsgBoxStyle.Exclamation)
+        End Try
+    End Sub
+
+End Class
--- FormularioInventario/fTransaccion.vb
+++ FormularioInventario/fTransaccion.vb
@@ -5,6 +5,9 @@ Public Class fTransaccion
     Dim conectar As New conexion
     Dim Da As New SqlDataAdapter
     Dim dt As New DataTable
+    Dim valorCombo As String
+    Dim ET As New transaccion
+    Dim datosTransaccion As New DatosTransaccion
 
 
     Private Sub BtnCargarDatos_Click(sender As Object, e As EventArgs) Handles btnCargarDatos.Click
@@ -28,7 +31,7 @@ Public Class fTransaccion
     Public Sub mostrarProductos()
         Try
             conectar.conectando()
-            Da = New SqlDataAdapter("select identificador,nombre,descripcion from producto", conectar.cnx)
+            Da = New SqlDataAdapter("select id,identificador,nombre,descripcion from producto", conectar.cnx)
             dt = New DataTable
             Da.Fill(dt)
             tablaProductos.DataSource = dt
@@ -47,13 +50,43 @@ Public Class fTransaccion
         Dim i As Integer
         i = tablaProductos.CurrentRow.Index
         TxtIdProducto.Text = tablaProductos.Item(0, i).Value()
-        TxtNombreProducto.Text = tablaProductos.Item(1, i).Value()
+        TxtNombreProducto.Text = tablaProductos.Item(2, i).Value()
     End Sub
 
     Private Sub TablaProvedores_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles tablaProvedores.CellContentClick
         Dim i As Integer
         i = tablaProvedores.CurrentRow.Index
         TxtIdProvedor.Text = tablaProvedores.Item(0, i).Value()
-        TxtNombreProvedor.Text = tablaProvedores.Item(0, i).Value()
+        TxtNombreProvedor.Text = tablaProvedores.Item(1, i).Value()
+    End Sub
+
+    Private Sub BtnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+        If TxtNombreProducto.Text <> "" And TxtNombreProvedor.Text <> "" And txtCantidad.Text <> "" And valorCombo <> "" Then
+            obtenerDatosDeCajas()
+            datosTransaccion.Registrar_transaccion(ET)
+            limpiarCajas()
+        Else
+            MsgBox("los campos no pueden quedar vacios", MsgBoxStyle.Exclamation)
+        End If
+    End Sub
+
+    Private Sub CbTipo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbTipo.SelectedIndexChanged
+        valorCombo = cbTipo.Text
+    End Sub
+
+    Private Sub obtenerDatosDeCajas()
+        If TxtIdProvedor.Text <> "" And TxtIdProducto.Text <> "" Then
+            ET.metodo_idProvedorTransaccion = TxtIdProvedor.Text
+            ET.metodo_idProductoTrasaccion = TxtIdProducto.Text
+        End If
+        ET.metodo_fechaTransaccion = tiempoControl.Value
+        ET.metodo_cantidadTransaccion = txtCantidad.Text
+        ET.metodo_tipoTransaccion = valorCombo
+    End Sub
+
+    Private Sub limpiarCajas()
+        TxtNombreProducto.Text = ""
+        TxtNombreProvedor.Text = ""
+        txtCantidad.Text = ""
     End Sub
 End Class
